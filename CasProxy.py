1cf427abb926d9eed8519467186a86e832f30d92
goni1772 <53024677+goni1772@users.noreply.github.com>
2020-02-27 09:41:20 +0900

    Update CasProxy.py

--- CasProxy.py
+++ CasProxy.py
@@ -5,22 +5,22 @@ import requests
 import logging as log
 import configparser
 
-config = configparser.ConfigParser()
-config.read('./config.ini')
-Service = config['SERVICE']
-Secure = config['KEY']
-Url = config['CASURL']
+config = configparser.ConfigParser() #configparser 객체 생성
+config.read('./config.ini') #config파일 리드
+Service = config['SERVICE'] #SERVICE필드 정의
+Secure = config['KEY'] #KEY필드 정의
+Url = config['CASURL'] # CAS Url 필드 정의
 
 CPID = Service['CPID'] #CAS 연동 시스템 CP계정 ID - 신청 후 할당
 CPPW = Service['CPPW']  #CAS 연동 시스템 CP PW - 신청 후 할당
-SERVICECODE = Service['CloudGame_ServiceCode']
+#SERVICECODE = Service['CloudGame_ServiceCode'] 
 CASECODE = Service['CloudGame_CaseCode'] #CAS 신청 CASECODE - 신청 후 할당
-INPUT = Service['input']
+INPUT = Service['input'] #CASECODE 발급시 신청한 Input값(ex. CTN or SUB_NO)
 
 KEY1 = Secure['KEY1'] #CAS 암호화 KEY값 - 신청 후 할당
-KEY2 = Secure['KEY2']
-KEY3 = Secure['KEY3']
-KEY4 = Secure['KEY4']
+KEY2 = Secure['KEY2'] #CAS 암호화 KEY값 - 신청 후 할당
+KEY3 = Secure['KEY3'] #CAS 암호화 KEY값 - 신청 후 할당
+KEY4 = Secure['KEY4'] #CAS 암호화 KEY값 - 신청 후 할당
 apiURL = Url['CASserver'] #CAS URL
 
 classpath = "./CASIFJAVACodec.jar"  # jar 파일 경로 정의
@@ -32,24 +32,32 @@ PyCasCryptoDecode = DefaultExtractor.CasCrypto.casCryptoDecode  # Decode Class.m
 log.basicConfig(filename='./log.txt', level=log.DEBUG) #로그파일 생성 및 저장
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/') # defualt router
 def default():
     return 'please send CTN!'
 
-@app.route('/CASINFO', methods=['POST'])
+@app.after_request
+def apply_caching(response): # response header 값 추가
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+@app.route('/CASINFO', methods=['POST']) #CASINFO router
 def getCASInfo():
-    InArg = request.form[INPUT]
+    Arg = request.get_json('body') #request 전문에서 body 값 추출
+    print(Arg)
+    InArg = Arg[INPUT] #body에서 CASECODE에 정의된 INPUT값 추출(ex. CTN or SUB_NO)
     print(InArg)
-    enCPPW = PyCasCryptoEncode(KEY1, KEY2, KEY3, KEY4, CPPW)
-    param = {'CPTYPE': 'I', 'CPID': CPID, 'CPPWD': enCPPW, 'CASECODE': CASECODE, INPUT : InArg}
-    enparam = parse.urlencode(param)
-    URL = apiURL + enparam
+    enCPPW = PyCasCryptoEncode(KEY1, KEY2, KEY3, KEY4, CPPW) #Password 암호화 method 호출(Python -> JVM -> jar)
+    param = {'CPTYPE': 'I', 'CPID': CPID, 'CPPWD': enCPPW, 'CASECODE': CASECODE, INPUT : InArg} #CAS로 요청할 URL 파라미터 생성
+    enparam = parse.urlencode(param) #생성된 파라미터 URL 인코딩
+    URL = apiURL + enparam #암호화->인코딩된 파라미터를 CAS URL과 조합
     print(URL)
-    CASinfo = requests.get(URL).headers
+    CASinfo = requests.get(URL).headers #최종 생성된 URL로 get 하여 header 값 수신
     print(CASinfo)
-    res = CASinfo['RESP']
-    deres = parse.parse_qs(res)
-    # final = deres['RESPMSG']
+    res = CASinfo['RESP'] #header에서 'RESP' 필드 추출
+    deres = parse.parse_qs(res) #응답값 URL 디코딩
+    # final = deres['RESPMSG'] 
     return(deres)
 
 if __name__ == '__main__':
